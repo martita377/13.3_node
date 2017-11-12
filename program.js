bc704a16c37247e61ccd5e35a7ef8e6244acc8fa
Marta P <mapa.startuj@gmail.com>
2017-11-12 20:59:15 +0100

    initial commit

---
+++ program.js
@@ -0,0 +1,24 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case '/lang':
+                process.stdout.write("Let's test language:\n");
+                console.log(process.env['LANG']);
+                break;
+            case '/node':
+                process.stdout.write('Node version:\n')
+                console.log(process.versions['node']);
+                break;
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+        }
+    }
+});
